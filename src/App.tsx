702d3bb5b888d367d69d4010dce4342870884eb6
Martijn van Exel <m@rtijn.org>
2025-09-04 09:09:20 -0600

    add maplibre component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import maplibregl from 'maplibre-gl'
+import 'maplibre-gl/dist/maplibre-gl.css'
 import './App.css'
 
 function App() {
+  const mapContainer = useRef<HTMLDivElement>(null)
+  const map = useRef<maplibregl.Map | null>(null)
+
+  useEffect(() => {
+    if (map.current) return
+
+    if (mapContainer.current) {
+      map.current = new maplibregl.Map({
+        container: mapContainer.current,
+        style: 'https://tiles.openfreemap.org/styles/liberty',
+        center: [5.2913, 52.1326],
+        zoom: 7
+      })
+    }
+
+    return () => {
+      map.current?.remove()
+    }
+  }, [])
+
   return (
-    <div className="App">
-      This is my map App
+    <div className="w-full h-screen">
+      <div ref={mapContainer} className="w-full h-full" />
     </div>)
 }
 
